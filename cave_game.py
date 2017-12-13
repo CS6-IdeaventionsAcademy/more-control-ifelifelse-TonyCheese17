4b4759c0b2f3bde206e5514685442fe67af3e208
TonyCheese11 <wyu@ideaventionsacadey.org>
2017-12-13 10:58:22 -0500

    def_practice and stuff
    the cave game doesn't work

--- cave_game.py
+++ cave_game.py
@@ -1,6 +1,22 @@
 # Tony Yu
 # 11/29/17
 # Explore a weird cave!
+ 
+def left_tunnel():
+    print("You walk into the left tunnel")
+    time.sleep(1)
+    print("You see a lake blocking your way. You also see a boat floating on it.")
+    lake_choice = input("Enter S for swim, B for boat, J for jump: ").upper()
+    return lake_choice
+
+def right_tunnel():
+    print("You walk into the right tunnel")
+    time.sleep(1)
+    print("You see a rope leading into a hole and also see light from a torch in the distance")
+    rope_choice = input("Enter R to climb down the rope or T to go to the torch: ").upper()
+    return rope_choice
+
+key = "open sesame"
 
 import time
 import random
@@ -24,12 +40,48 @@ print()
 tunnel_choice = input("You see the cave branch off into two tunnels.(type L for left, R for right): ").upper()
 
 if tunnel_choice == "L":
-    print("You walk into the left tunnel")
-    time.sleep(1)
-    lake_choice = input("You see a lake blocking your way. You also see a boat floating on it.(S for swim, B for boat, J for jump")
+    lake_choice = left_tunnel()
+    if lake_choice == "S" or lake_choice == "SWIM":
+        print("You jump in the freezing water and start swimming")
+        print("You realize you never learned how to swim...")
+        time.sleep(1.2)
+        print("But you start doggy paddling and realize you can swim this way")
+        # Remember to add winning text
+    elif lake_choice == "B" or lake_choice == "BOAT":
+        print("You climb into the boat and realize you have no paddle")
+        print("The boat starts drifting away from the edge...")
+        time.sleep(1.4)
+        boat_choice = input("What do you do?(E for escape the boat or S for stay): ").upper()
+        if boat_choice == "E": 
+            print("You try to jump ashore but fail and drown")
+        elif boat_choice == "S":
+            print("You stay there, and a giant rubber duck starts drifting towards you and crush yo face")
+        elif boat_choice == ("OPEN SESAME"):
+            print("""Once upon a time, there was a player. He wanted to find all the secrets of the game.
+He decided he wanted to say open seasame! He saw this text, and died. THE END """)
+            time.sleep(5)
+            print("YOU DIED")
+        else:
+            print("NOPE")
+    elif lake_choice == "J" or lake_choice == "JUMP":
+        print("You try to jump over the lake but fall in and drown")
+    else:
+        print("THAT IS NOT VALID OPTION")
+        print("SELF DESTRUCTING COMPOTER",end='')
+        time.sleep(0.8)
+        print(".",end='')
+        time.sleep(0.8)
+        print(".",end='')
+        time.sleep(0.8)
+        print(".",end='')
+        print("YOUR COMPOTER JUST EXPLODED")
 elif tunnel_choice == "R":
-    print("You walk into the right tunnel")
-else:
+        rope_choice = right_tunnel()
+        
+    if rope_choice == "R":
+        
+    
+    else:
     print("Why didn't you choose a tunnel?")
     print("You just stand there for a few minutes until a cow comes and digests you") 
 
---
+++ def_practice.py
@@ -0,0 +1,49 @@
+# Tony Yu
+# 12/12/17
+# Def practice
+
+import math
+
+##def cheese():
+##    print("I LIKE CHEESE "*100)
+##
+##cheese()
+
+##def temp_c(temp_f):
+##    '''Converts a temperature in Fahrenheit to Celsius'''
+##    answer = (temp_f - 32) * (5/9)
+##    return answer
+##
+##while "0" == "0":
+##    t_in_f = input("Please enter a temperature in Fahrenheit: ")
+##    t_in_f = float(t_in_f)
+##    t_in_c = temp_c(t_in_f)
+##
+##    print("That temperature in Celsius is",t_in_c) 
+
+##def vol_sphere(radius):
+##    '''Calculates the volume of a sphere'''
+##    volume = (4/3)*math.pi*r**3
+##    return volume
+##
+##r = input("Please enter the radius of the sphere you want to know the volume of: ")
+##r = float(r)
+##v_sphere = vol_sphere(r)
+##if v_sphere > 100:
+##    print("Thats a huge sphere!")
+##else:
+##    print("Thats a small sphere!")
+##
+##print("The volume of the sphere is", v_sphere)
+
+def hamburger(wish):
+    if wish.lower() == "no":
+        reply = "WHY DO YOU NOT LIKE HAMBURGERS"
+    elif wish.lower() == "yes":
+        reply = "Yay! Hamburgers are the best"
+    else:
+        reply = "What?"
+    return reply
+        
+wish = input("Do you like hamburgers: ")
+print(hamburger(wish))
